feec7f0058e741514961a8aff61f7c1750ac5368
Umut Emre Sarikaya <61623237+UmutEmreSarikaya@users.noreply.github.com>
2023-11-22 17:57:27 +0300

    Fixed StringExtensions.kt

--- app/src/main/java/com/umut/pokedexapp/util/StringExtensions.kt
+++ app/src/main/java/com/umut/pokedexapp/util/StringExtensions.kt
@@ -1,8 +1,7 @@
 package com.umut.pokedexapp.util
 
-class StringExtensions {
-    fun String.extractIdFromUrl(): Int {
-        val parts = this.split("/")
-        return parts[parts.size - 2].toInt()
-    }
+
+fun String.extractIdFromUrl(): Int {
+    val parts = this.split("/")
+    return parts[parts.size - 2].toInt()
 }
